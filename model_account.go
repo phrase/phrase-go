ebac1f576ad132ff6bba4b590f725092f7a97782
Phrase <support@phrase.com>
2021-03-19 06:35:22 +0000

    Deploying from  phrase/openapi@0e889a08

--- model_account.go
+++ model_account.go
@@ -6,10 +6,11 @@ import (
 
 // Account struct for Account
 type Account struct {
-	Id        string    `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	Slug      string    `json:"slug,omitempty"`
-	Company   string    `json:"company,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	Id             string    `json:"id,omitempty"`
+	Name           string    `json:"name,omitempty"`
+	Slug           string    `json:"slug,omitempty"`
+	Company        string    `json:"company,omitempty"`
+	CreatedAt      time.Time `json:"created_at,omitempty"`
+	UpdatedAt      time.Time `json:"updated_at,omitempty"`
+	CompanyLogoUrl string    `json:"company_logo_url,omitempty"`
 }
--- model_account_details.go
+++ model_account_details.go
@@ -6,10 +6,11 @@ import (
 
 // AccountDetails struct for AccountDetails
 type AccountDetails struct {
-	Id        string    `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	Slug      string    `json:"slug,omitempty"`
-	Company   string    `json:"company,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	Id             string    `json:"id,omitempty"`
+	Name           string    `json:"name,omitempty"`
+	Slug           string    `json:"slug,omitempty"`
+	Company        string    `json:"company,omitempty"`
+	CreatedAt      time.Time `json:"created_at,omitempty"`
+	UpdatedAt      time.Time `json:"updated_at,omitempty"`
+	CompanyLogoUrl string    `json:"company_logo_url,omitempty"`
 }
