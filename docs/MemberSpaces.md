17e90371f225dbf97f5b95b713cdbb824d5adc37
Phrase <support@phrase.com>
2021-05-11 09:22:09 +0000

    Deploying from  phrase/openapi@c5238231

--- docs/MemberSpaces.md
+++ docs/MemberSpaces.md
@@ -6,8 +6,8 @@ Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **Id** | **string** |  | [optional] 
 **Name** | **string** |  | [optional] 
-**CreatedAt** | [**interface{}**](.md) |  | [optional] 
-**UpdatedAt** | [**interface{}**](.md) |  | [optional] 
+**CreatedAt** | [**time.Time**](time.Time.md) |  | [optional] 
+**UpdatedAt** | [**time.Time**](time.Time.md) |  | [optional] 
 **ProjectsCount** | **int32** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- model_member_spaces.go
+++ model_member_spaces.go
@@ -1,10 +1,14 @@
 package phrase
 
+import (
+	"time"
+)
+
 // MemberSpaces struct for MemberSpaces
 type MemberSpaces struct {
-	Id            string      `json:"id,omitempty"`
-	Name          string      `json:"name,omitempty"`
-	CreatedAt     interface{} `json:"created_at,omitempty"`
-	UpdatedAt     interface{} `json:"updated_at,omitempty"`
-	ProjectsCount int32       `json:"projects_count,omitempty"`
+	Id            string    `json:"id,omitempty"`
+	Name          string    `json:"name,omitempty"`
+	CreatedAt     time.Time `json:"created_at,omitempty"`
+	UpdatedAt     time.Time `json:"updated_at,omitempty"`
+	ProjectsCount int32     `json:"projects_count,omitempty"`
 }
