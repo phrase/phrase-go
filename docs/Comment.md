3c3c3de954fed5c09eb7d62f2651d9820f61a13c
Phrase <support@phrase.com>
2021-06-04 07:37:41 +0000

    Deploying from  phrase/openapi@d6deaf9d

--- docs/Comment.md
+++ docs/Comment.md
@@ -9,6 +9,7 @@ Name | Type | Description | Notes
 **User** | [**UserPreview**](user_preview.md) |  | [optional] 
 **CreatedAt** | [**time.Time**](time.Time.md) |  | [optional] 
 **UpdatedAt** | [**time.Time**](time.Time.md) |  | [optional] 
+**MentionedUsers** | [**[]UserPreview**](user_preview.md) |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- model_comment.go
+++ model_comment.go
@@ -6,9 +6,10 @@ import (
 
 // Comment struct for Comment
 type Comment struct {
-	Id        string      `json:"id,omitempty"`
-	Message   string      `json:"message,omitempty"`
-	User      UserPreview `json:"user,omitempty"`
-	CreatedAt time.Time   `json:"created_at,omitempty"`
-	UpdatedAt time.Time   `json:"updated_at,omitempty"`
+	Id             string        `json:"id,omitempty"`
+	Message        string        `json:"message,omitempty"`
+	User           UserPreview   `json:"user,omitempty"`
+	CreatedAt      time.Time     `json:"created_at,omitempty"`
+	UpdatedAt      time.Time     `json:"updated_at,omitempty"`
+	MentionedUsers []UserPreview `json:"mentioned_users,omitempty"`
 }
