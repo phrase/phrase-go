5fa492bde888676b7293740e5a5087603325c756
Phrase <support@phrase.com>
2021-09-08 13:03:17 +0000

    Deploying from  phrase/openapi@60c8c91c

--- model_translations_verify_parameters.go
+++ model_translations_verify_parameters.go
@@ -4,10 +4,8 @@ package phrase
 type TranslationsVerifyParameters struct {
 	// specify the branch to use
 	Branch string `json:"branch,omitempty"`
+	// specify the locale of the translations to be verified
+	LocaleId string `json:"locale_id,omitempty"`
 	// Specify a query to find translations by content (including wildcards).<br><br> The following qualifiers are supported in the query:<br> <ul>   <li><code>id:translation_id,...</code> for queries on a comma-separated list of ids</li>   <li><code>tags:XYZ</code> for tags on the translation</li>   <li><code>unverified:{true|false}</code> for verification status</li>   <li><code>excluded:{true|false}</code> for exclusion status</li>   <li><code>updated_at:{>=|<=}2013-02-21T00:00:00Z</code> for date range queries</li> </ul> Find more examples <a href=\"#overview--usage-examples\">here</a>.
 	Q string `json:"q,omitempty"`
-	// Sort criteria. Can be one of: key_name, created_at, updated_at.
-	Sort string `json:"sort,omitempty"`
-	// Order direction. Can be one of: asc, desc.
-	Order string `json:"order,omitempty"`
 }
