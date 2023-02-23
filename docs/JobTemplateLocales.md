d80f1fc1902661ff32392bff7e5dd061d6a8f507
Phrase <support@phrase.com>
2023-02-23 12:23:07 +0000

    Deploying from  phrase/openapi@d9129f2b

--- docs/JobTemplateLocales.md
+++ docs/JobTemplateLocales.md
@@ -7,8 +7,8 @@ Name | Type | Description | Notes
 **Id** | **string** |  | [optional] 
 **JobTemplate** | [**JobTemplatePreview**](job_template_preview.md) |  | [optional] 
 **Locale** | [**LocalePreview**](locale_preview.md) |  | [optional] 
-**Users** | [**[]Items**](items.md) |  | [optional] 
-**Teams** | [**[]Items**](items.md) |  | [optional] 
+**Users** | [**[]LocaleUserPreview**](locale_user_preview.md) |  | [optional] 
+**Teams** | [**[]LocaleTeamPreview**](locale_team_preview.md) |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- model_job_template_locales.go
+++ model_job_template_locales.go
@@ -2,9 +2,9 @@ package phrase
 
 // JobTemplateLocales struct for JobTemplateLocales
 type JobTemplateLocales struct {
-	Id          string             `json:"id,omitempty"`
-	JobTemplate JobTemplatePreview `json:"job_template,omitempty"`
-	Locale      LocalePreview      `json:"locale,omitempty"`
-	Users       []Items            `json:"users,omitempty"`
-	Teams       []Items            `json:"teams,omitempty"`
+	Id          string              `json:"id,omitempty"`
+	JobTemplate JobTemplatePreview  `json:"job_template,omitempty"`
+	Locale      LocalePreview       `json:"locale,omitempty"`
+	Users       []LocaleUserPreview `json:"users,omitempty"`
+	Teams       []LocaleTeamPreview `json:"teams,omitempty"`
 }
