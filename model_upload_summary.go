7dfc62fdd147db72a5f9e998dacf0ff58a09d427
Phrase <support@phrase.com>
2026-06-11 10:28:55 +0000

    Deploying from  phrase/openapi@c42eae56

--- model_upload_summary.go
+++ model_upload_summary.go
@@ -5,7 +5,7 @@ type UploadSummary struct {
 	LocalesCreated         int32 `json:"locales_created,omitempty"`
 	TranslationKeysCreated int32 `json:"translation_keys_created,omitempty"`
 	TranslationKeysUpdated int32 `json:"translation_keys_updated,omitempty"`
-	// The number of translation keys in the project that were not mentioned in the upload.  Note: this field is not calculated (and shows 0) if the upload contains more than 10,000 keys.
+	// The number of translation keys in the project that were not mentioned in the upload.  Note: this field is not calculated (and shows 0) if the upload contains more than 100,000 keys.
 	TranslationKeysUnmentioned int32 `json:"translation_keys_unmentioned,omitempty"`
 	TranslationsCreated        int32 `json:"translations_created,omitempty"`
 	TranslationsUpdated        int32 `json:"translations_updated,omitempty"`
